464af8c99420ba23e730d22db33e4e27f8f4b81c
SyedSumaimaly <syedsumaimaly08@gmail.com>
2024-09-19 21:39:56 +0500

    add and delete functionality done

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,11 +1,11 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [FormsModule, NgFor],
+  imports: [FormsModule, NgFor, NgIf],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
@@ -18,7 +18,21 @@ export class TodoListComponent {
 
 
 
-onSubmit(form: NgForm){
+  onSubmit(form: NgForm) {
+    console.log(form)
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      isCompleted: false
+    })
 
-}
+    form.reset();
+  }
+
+  onDelete(index: number) {
+    console.log(index)
+
+    this.taskArray.splice(index, 1)
+
+  }
+
 }
